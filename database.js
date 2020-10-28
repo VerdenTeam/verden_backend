418a72f7da9395d2253deb3bb83d80c4a32cc608
Dash166 <joseluis_1555@hotmail.com>
2020-10-28 16:55:26 -0500

    fix package.json

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-module.exports = mongoose.connect("mongodb://localhost:27017/verden", {
+module.exports = mongoose.connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
